9423398089ead72d53e6ed5217d4a96b71cc83a2
Craig Condit <ccondit@randomcoder.org>
2007-10-22 23:21:10 +0000

    Added system tray code; commented out until it works properly.
    git-svn-id: https://randomcoder.org/repos/public/randomcoder-proxy-client/trunk@780 5bee6cb3-3d18-0410-8c93-a642edd49b48

--- src/main/java/com/randomcoder/proxy/client/ProxyClient.java
+++ src/main/java/com/randomcoder/proxy/client/ProxyClient.java
@@ -5,6 +5,34 @@ import java.net.*;
 
 import org.apache.log4j.Logger;
 
+/**
+ * HTTP proxy client.
+ * 
+ * <pre>
+ * Copyright (c) 2007, Craig Condit. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *     
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS &quot;AS IS&quot;
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * </pre>
+ */
 public class ProxyClient
 {
 	private static final Logger logger = Logger.getLogger(ProxyClient.class);
@@ -15,6 +43,18 @@ public class ProxyClient
 	private final int localPort;
 	private final Authenticator auth;
 	
+	/**
+	 * Creates a new proxy client.
+	 * 
+	 * @param proxyUrl
+	 *            base URL of remote proxy
+	 * @param remoteHost
+	 *            host to connect to on remote side
+	 * @param remotePort
+	 *            port to connect to on remote side
+	 * @param localPort
+	 *            local port to listen on
+	 */
 	public ProxyClient(String proxyUrl, String remoteHost, int remotePort, int localPort)
 	{
 		this.proxyUrl = proxyUrl;
@@ -24,6 +64,12 @@ public class ProxyClient
 		auth = new SwingAuthenticator();
 	}
 	
+	/**
+	 * Listens for incoming connections.
+	 * 
+	 * @throws IOException
+	 *             if an I/O error occurs
+	 */
 	public void listen() throws IOException
 	{
 		ServerSocket ss = new ServerSocket();
@@ -56,6 +102,12 @@ public class ProxyClient
 		}
 	}
 	
+	/**
+	 * Main entry point for application.
+	 * 
+	 * @param args
+	 *            unused
+	 */
 	public static void main(String[] args)
 	{
 		try
@@ -76,6 +128,61 @@ public class ProxyClient
 			
 			ProxyClient client = new ProxyClient(proxyUrl, remoteHost, remotePort, localPort);	
 			client.listen();
+			
+//			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+//			
+//			if (SystemTrayWrapper.isSupported())
+//			{
+//				// system tray available
+//				SystemTrayWrapper tray = SystemTrayWrapper.getSystemTray();
+//				
+//				BufferedImage image = ImageIO.read(ProxyClient.class.getResourceAsStream("/socket.png"));
+//				
+//				PopupMenu popup = new PopupMenu();
+//				
+//				popup.setFont(new JMenuItem("test").getFont());
+//				
+//				MenuItem configure = new MenuItem("Configure");
+//				configure.addActionListener(new ActionListener()
+//				{
+//					public void actionPerformed(ActionEvent e)
+//					{
+//						// TODO open configuration screen
+//						System.err.println("Configure chosen");
+//					}
+//				});				
+//				popup.add(configure);
+//				configure.setFont(popup.getFont().deriveFont(Font.BOLD));
+//				
+//				popup.addSeparator();
+//				
+//				MenuItem exit = new MenuItem("Exit");
+//				exit.addActionListener(new ActionListener()
+//				{
+//					public void actionPerformed(ActionEvent e)
+//					{
+//						System.exit(0);
+//					}
+//				});
+//				popup.add(exit);
+//				TrayIconWrapper icon = new TrayIconWrapper(image, "Disconnected", popup);
+//				icon.addActionListener(new ActionListener()
+//				{
+//					public void actionPerformed(ActionEvent e)
+//					{
+//						// TODO open configuration screen
+//						System.err.println("Default action chosen - configure");
+//					}
+//				});
+//				icon.setImageAutoSize(true);
+//				tray.add(icon);
+//				
+//			}
+//			else
+//			{
+//				System.err.println("No system tray found");
+//			}
+			
 		}
 		catch (Throwable t)
 		{
