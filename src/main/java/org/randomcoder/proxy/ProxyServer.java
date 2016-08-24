5520afa4abe3e6b6a42f42eb3b3d9a45a63ca58e
Craig Condit <ccondit@randomcoder.org>
2016-08-24 16:25:00 -0500

    Updated to Jetty 9.3 / HTTP2

--- src/main/java/org/randomcoder/proxy/ProxyServer.java
+++ src/main/java/org/randomcoder/proxy/ProxyServer.java
@@ -1,30 +1,60 @@
 package org.randomcoder.proxy;
 
-import java.io.*;
+import java.io.Console;
+import java.io.File;
+import java.io.IOException;
 import java.security.SecureRandom;
-import java.util.*;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
 
-import org.eclipse.jetty.http.security.UnixCrypt;
-import org.eclipse.jetty.server.*;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.nio.SelectChannelConnector;
-import org.randomcoder.proxy.handlers.*;
+import org.eclipse.jetty.http2.server.HTTP2CServerConnectionFactory;
+import org.eclipse.jetty.rewrite.handler.HeaderPatternRule;
+import org.eclipse.jetty.rewrite.handler.RewriteHandler;
+import org.eclipse.jetty.rewrite.handler.RuleContainer;
+import org.eclipse.jetty.server.ConnectionFactory;
+import org.eclipse.jetty.server.ForwardedRequestCustomizer;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.server.handler.SecuredRedirectHandler;
+import org.eclipse.jetty.util.security.UnixCrypt;
+import org.eclipse.jetty.util.thread.QueuedThreadPool;
+import org.eclipse.jetty.util.thread.ScheduledExecutorScheduler;
+import org.randomcoder.proxy.handlers.AuthHandler;
+import org.randomcoder.proxy.handlers.BasicAuthHandler;
+import org.randomcoder.proxy.handlers.ConnectHandler;
+import org.randomcoder.proxy.handlers.DisconnectHandler;
+import org.randomcoder.proxy.handlers.PingHandler;
+import org.randomcoder.proxy.handlers.ReceiveHandler;
+import org.randomcoder.proxy.handlers.SendHandler;
+import org.randomcoder.proxy.handlers.StatusHandler;
 import org.randomcoder.proxy.support.EndpointTracker;
 
 /**
  * HTTP tunneling proxy server.
  */
-public class ProxyServer
-{
+public class ProxyServer {
 	private static final int DEFAULT_PORT = 9999;
 	private static final String DEFAULT_PATH = "/proxy";
 	private static final String DEFAULT_HOST = "127.0.0.1";
 	private static final String DEFAULT_AUTH = "/srv/randomcoder-proxy/etc/passwd";
+	private static final String DEFAULT_HTTP_FORWARDED = "false";
+	private static final String DEFAULT_HTTPS_FORCED = "false";
+	private static final String DEFAULT_STS_MAX_AGE = "0";
 
 	private static final String HOST_PARAM = "proxy.host";
 	private static final String PORT_PARAM = "proxy.port";
 	private static final String PATH_PARAM = "proxy.path";
 	private static final String AUTH_PARAM = "auth.file";
+	private static final String HTTP_FORWARDED_PARAM = "http.forwarded";
+	private static final String HTTPS_FORCED_PARAM = "https.forced";
+	private static final String STS_MAX_AGE_PARAM = "sts.max.age";
 
 	private final EndpointTracker tracker;
 	private final Server server;
@@ -40,27 +70,53 @@ public class ProxyServer
 	 *            base URL
 	 * @param passwdFile
 	 *            password file
+	 * @param forward
+	 *            whether to do X-Forwarded-{For,Proto} handling
+	 * @param forceHttps
+	 *            whether to force a redirect to HTTPS
+	 * @param sendSts
+	 *            whether to send Strict-Transport-Security header
+	 * @param stsMaxAge
+	 *            maximum age of STS header
 	 * @throws IOException
 	 *             if an error occurs
 	 */
-	public ProxyServer(String host, int port, String context, File passwdFile)
-			throws IOException
-	{
+	public ProxyServer(String host, int port, String context, File passwdFile, boolean forward, boolean forceHttps,
+			boolean sendSts, long stsMaxAge) throws IOException {
 		tracker = new EndpointTracker();
-		server = new Server();
 
-		if (context == "/")
-		{
+		if (context == "/") {
 			context = "";
 		}
 
-		SelectChannelConnector con = new SelectChannelConnector();
-		con.setPort(port);
-		con.setHost(host);
+		QueuedThreadPool threadPool = new QueuedThreadPool(10, 4, 60000);
 
-		server.setConnectors(new Connector[] { con });
+		server = new Server(threadPool);
+		server.addBean(new ScheduledExecutorScheduler());
 
-		HandlerList handlers = new HandlerList();
+		HttpConfiguration httpConfig = new HttpConfiguration();
+		httpConfig.setSecurePort(443);
+
+		if (forward) {
+			httpConfig.addCustomizer(new ForwardedRequestCustomizer());
+		}
+
+		HandlerCollection handlers = new HandlerCollection();
+
+		handlers.addHandler(new SecuredRedirectHandler());
+
+		if (sendSts) {
+			RewriteHandler rewriteHandler = new RewriteHandler();
+			RuleContainer rules = new RuleContainer();
+
+			HeaderPatternRule stsRule = new HeaderPatternRule();
+			stsRule.setPattern("/*");
+			stsRule.setName("Strict-Transport-Security");
+			stsRule.setValue("max-age=" + new DecimalFormat("####################").format(stsMaxAge));
+			rules.addRule(stsRule);
+			rewriteHandler.setRuleContainer(rules);
+			handlers.addHandler(rewriteHandler);
+		}
 
 		// authentication handler must be first
 		handlers.addHandler(new BasicAuthHandler(passwdFile));
@@ -75,6 +131,24 @@ public class ProxyServer
 		handlers.addHandler(new StatusHandler(context, tracker));
 
 		server.setHandler(handlers);
+
+		List<ConnectionFactory> connectionFactories = new ArrayList<>();
+
+		// http/1.1 connector
+		connectionFactories.add(new HttpConnectionFactory(httpConfig));
+
+		// h2c connector
+		HTTP2CServerConnectionFactory http2cFactory = new HTTP2CServerConnectionFactory(httpConfig);
+		http2cFactory.setMaxConcurrentStreams(-1);
+		http2cFactory.setInitialStreamRecvWindow(65535);
+		connectionFactories.add(http2cFactory);
+
+		ServerConnector httpConnector = new ServerConnector(server, 1, -1,
+				connectionFactories.toArray(new ConnectionFactory[] {}));
+		httpConnector.setHost(host);
+		httpConnector.setPort(port);
+
+		server.addConnector(httpConnector);
 	}
 
 	/**
@@ -83,8 +157,7 @@ public class ProxyServer
 	 * @throws Exception
 	 *             if an error occurs
 	 */
-	public void start() throws Exception
-	{
+	public void start() throws Exception {
 		server.start();
 	}
 
@@ -94,8 +167,7 @@ public class ProxyServer
 	 * @throws Exception
 	 *             if an error occurs
 	 */
-	public void stop() throws Exception
-	{
+	public void stop() throws Exception {
 		server.stop();
 		tracker.destroy();
 	}
@@ -108,18 +180,14 @@ public class ProxyServer
 	 * @throws Exception
 	 *             if an error occurs
 	 */
-	public static void main(String[] args) throws Exception
-	{
+	public static void main(String[] args) throws Exception {
 		LinkedList<String> params = new LinkedList<String>(Arrays.asList(args));
-		if (params.size() > 0)
-		{
+		if (params.size() > 0) {
 			String command = params.remove();
-			if ("encrypt".equals(command) && params.size() == 1)
-			{
+			if ("encrypt".equals(command) && params.size() == 1) {
 				String username = params.remove();
 				Console console = System.console();
-				if (console == null)
-				{
+				if (console == null) {
 					System.err.println("Error: Console not available");
 					return;
 				}
@@ -130,8 +198,7 @@ public class ProxyServer
 				Random random = new SecureRandom();
 				String digits = new String("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwyxz");
 				StringBuilder buf = new StringBuilder();
-				for (int i = 0; i < 16; i++)
-				{
+				for (int i = 0; i < 16; i++) {
 					buf.append(digits.charAt(random.nextInt(digits.length())));
 				}
 				System.out.println(username + ":" + UnixCrypt.crypt(new String(password), buf.toString()));
@@ -145,28 +212,27 @@ public class ProxyServer
 		int port = Integer.parseInt(System.getProperty(PORT_PARAM, Integer.toString(DEFAULT_PORT)));
 		String context = System.getProperty(PATH_PARAM, DEFAULT_PATH);
 		String passwd = System.getProperty(AUTH_PARAM, DEFAULT_AUTH);
+		boolean forwarded = Boolean.parseBoolean(System.getProperty(HTTP_FORWARDED_PARAM, DEFAULT_HTTP_FORWARDED));
+		boolean forceHttps = Boolean.parseBoolean(System.getProperty(HTTPS_FORCED_PARAM, DEFAULT_HTTPS_FORCED));
+		long stsMaxAge = Long.parseLong(System.getProperty(STS_MAX_AGE_PARAM, DEFAULT_STS_MAX_AGE), 10);
+		boolean sendSts = (stsMaxAge > 0L);
 
 		File passwdFile = new File(passwd);
-		if (!passwdFile.exists())
-		{
+		if (!passwdFile.exists()) {
 			System.err.println("Error: Password file " + passwdFile.getAbsolutePath() + " does not exist.");
 			return;
 		}
 
-		final ProxyServer proxy = new ProxyServer(host, port, context, passwdFile);
+		final ProxyServer proxy = new ProxyServer(host, port, context, passwdFile, forwarded, forceHttps, sendSts,
+				stsMaxAge);
 		proxy.start();
 
-		Runtime.getRuntime().addShutdownHook(new Thread()
-		{
+		Runtime.getRuntime().addShutdownHook(new Thread() {
 			@Override
-			public void run()
-			{
-				try
-				{
+			public void run() {
+				try {
 					proxy.stop();
-				}
-				catch (Exception e)
-				{
+				} catch (Exception e) {
 					e.printStackTrace();
 					System.exit(1);
 				}
@@ -177,11 +243,11 @@ public class ProxyServer
 	/**
 	 * Displays server usage.
 	 */
-	public static void usage()
-	{
+	public static void usage() {
 		System.err.println("Usage: " + ProxyServer.class.getName() + " [command]");
 		System.err.println("  where command is one of the following:");
-		System.err.println("    encrypt <username> -- prompts for a password and writes a password entry to standard out.");
+		System.err.println(
+				"    encrypt <username> -- prompts for a password and writes a password entry to standard out.");
 		System.err.println("    usage -- display this message");
 		System.err.println();
 		System.err.println("  If no command is entered, the proxy server will start up.");
@@ -191,5 +257,10 @@ public class ProxyServer
 		System.err.println("    " + PORT_PARAM + " -- port to listen on [" + DEFAULT_PORT + "]");
 		System.err.println("    " + PATH_PARAM + " -- context path [" + DEFAULT_PATH + "]");
 		System.err.println("    " + AUTH_PARAM + " -- password file [" + DEFAULT_AUTH + "]");
+		System.err.println("    " + HTTP_FORWARDED_PARAM + " -- whether to honor X-Forwarded-{For,Proto} headers ["
+				+ DEFAULT_HTTP_FORWARDED + "]");
+		System.err.println("    " + HTTPS_FORCED_PARAM + " -- force HTTPS redirects [" + DEFAULT_HTTPS_FORCED + "]");
+		System.err.println(
+				"    " + STS_MAX_AGE_PARAM + " -- max age for Strict-Transport-Security header [0 (disabled)]");
 	}
 }
--- src/main/java/org/randomcoder/proxy/handlers/BasicAuthHandler.java
+++ src/main/java/org/randomcoder/proxy/handlers/BasicAuthHandler.java
@@ -1,29 +1,37 @@
 package org.randomcoder.proxy.handlers;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.*;
-import org.eclipse.jetty.http.security.*;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-
+import org.eclipse.jetty.util.B64Code;
+import org.eclipse.jetty.util.security.UnixCrypt;
 import org.randomcoder.proxy.support.CurrentUser;
 
 /**
- * BASIC authentication handler. 
+ * BASIC authentication handler.
  */
-public class BasicAuthHandler extends AbstractHandler
-{
+public class BasicAuthHandler extends AbstractHandler {
 	private static final Logger logger = LogManager.getLogger(BasicAuthHandler.class);
-	
+
 	private static final String REALM = "Proxy";
-	
+
 	private final Map<String, String> userMap;
-	
+
 	/**
 	 * Creates a new BASIC auth handler.
 	 * 
@@ -32,42 +40,43 @@ public class BasicAuthHandler extends AbstractHandler
 	 * @throws IOException
 	 *             if an error occurs
 	 */
-	public BasicAuthHandler(File passwdFile) throws IOException
-	{
+	public BasicAuthHandler(File passwdFile) throws IOException {
 		Map<String, String> map = new HashMap<String, String>();
-		
+
 		FileReader fr = null;
 		BufferedReader br = null;
-		try
-		{
+		try {
 			fr = new FileReader(passwdFile);
 			br = new BufferedReader(fr);
 			String line = null;
-			while ((line = br.readLine()) != null)
-			{
+			while ((line = br.readLine()) != null) {
 				String[] parts = line.split(":");
-				if (parts.length >= 2)
-				{
+				if (parts.length >= 2) {
 					map.put(parts[0], parts[1]);
 				}
-			}				
-		}
-		finally
-		{
-			if (br != null) try { br.close(); } catch (Exception ignored) {}
-			if (fr != null) try { fr.close(); } catch (Exception ignored) {}
+			}
+		} finally {
+			if (br != null)
+				try {
+					br.close();
+				} catch (Exception ignored) {
+				}
+			if (fr != null)
+				try {
+					fr.close();
+				} catch (Exception ignored) {
+				}
 		}
 
 		logger.info("Initialized user store with " + map.size() + " entries");
 		userMap = Collections.unmodifiableMap(map);
 	}
-	
+
 	@Override
-	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException
-	{
+	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 		String username = getUser(request.getHeader("Authorization"));
-		if (username == null)
-		{
+		if (username == null) {
 			CurrentUser.logout();
 			response.setHeader("WWW-Authenticate", "Basic realm=\"" + REALM + "\"");
 			response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
@@ -76,48 +85,38 @@ public class BasicAuthHandler extends AbstractHandler
 		}
 		CurrentUser.login(username);
 	}
-	
-	private String getUser(String auth)
-	{
-		try
-		{
-			if (auth == null)
-			{
+
+	private String getUser(String auth) {
+		try {
+			if (auth == null) {
 				return null;
 			}
-			
+
 			String[] parts = auth.split(" ");
-			if (parts.length < 2)
-			{
+			if (parts.length < 2) {
 				return null;
 			}
-			
-			if (!("basic".equals(parts[0].toLowerCase(Locale.US))))
-			{
+
+			if (!("basic".equals(parts[0].toLowerCase(Locale.US)))) {
 				return null;
 			}
-			
-			String token = B64Code.decode(parts[1]);
+
+			String token = B64Code.decode(parts[1], Charset.forName("UTF-8"));
 			String[] userpass = token.split(":");
-			if (userpass.length != 2)
-			{
+			if (userpass.length != 2) {
 				return null;
 			}
-			
+
 			String hash = userMap.get(userpass[0]);
-			if (hash == null)
-			{
+			if (hash == null) {
 				return null;
 			}
-			
-			if (!(hash.equals(UnixCrypt.crypt(userpass[1], hash))))
-			{
+
+			if (!(hash.equals(UnixCrypt.crypt(userpass[1], hash)))) {
 				return null;
 			}
 			return userpass[0];
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			logger.warn("Invalid auth token received", e);
 			return null;
 		}
